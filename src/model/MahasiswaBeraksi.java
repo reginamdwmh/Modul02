bd085d9ab85a19a384c7012519737249adfeef56
Ariffudholi <reginamudawamah@gmail.com>
2021-06-06 11:49:11 +0800

    Membuat Method

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -18,7 +18,7 @@ public class MahasiswaBeraksi {
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa.npm);
         System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
-        
+
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "20180175";
         mahasiswa2.nama = "Mudawamah";
@@ -27,6 +27,10 @@ public class MahasiswaBeraksi {
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
         System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+
+        mahasiswa.tampilkanAtribut();
+        mahasiswa.menyapa();
+        System.out.println("Usiaku : " + mahasiswa.hitungUsia() + " tahun");
 
     }
 }
